5ce9552f112d3685784b0e10daeac2fddc7b82b3
Eric Dobson <eric.n.dobson@gmail.com>
2015-06-20 23:24:14 -0700

    Fix some memory issues in the client.

--- client.rkt
+++ client.rkt
@@ -18,31 +18,31 @@
 (define (run cq)
   (define deadline (gpr-now))
   (set-gpr-timespec-tv_sec! deadline (+ (gpr-timespec-tv_sec deadline) 1))
-  
+
   (define call (grpc-channel-create-call chan cq "/grpc.testing.TestService/EmptyCall" "localhost" deadline))
-  
-  
+
+
   (define recv-metadata (make-grpc-metadata-array 0 0 #f))
   (grpc-metadata-array-init recv-metadata)
 
   (define send-message-slice (gpr-slice-from-copied-buffer #"\x08\x00\x10\x12"))
   (define send-message-buffer (grpc-raw-byte-buffer-create send-message-slice 1))
 
-  
-  (define recv-message-buffer-ptr (malloc _pointer))
+
+  (define recv-message-buffer-ptr (malloc _pointer 'raw))
   (ptr-set! recv-message-buffer-ptr _pointer #f)
-  
+
   (define recv-trailing-metadata (make-grpc-metadata-array 0 0 #f))
   (grpc-metadata-array-init recv-trailing-metadata)
-  (define recv-status_code (malloc _int))
+  (define recv-status_code (malloc _int 'raw))
   (ptr-set! recv-status_code _int 0)
-  (define recv-status_details (malloc _pointer))
+  (define recv-status_details (malloc _pointer 'raw))
   (ptr-set! recv-status_details _pointer #f)
-  
-  (define recv-status_details_capacity (malloc _size_t))
+
+  (define recv-status_details_capacity (malloc _size_t 'raw))
   (ptr-set! recv-status_details_capacity _size_t 0)
   (define recv-status (make-grpc-recv_status_on_client recv-trailing-metadata recv-status_code recv-status_details recv-status_details_capacity))
-  
+
 
 
   (define ops
@@ -55,7 +55,7 @@
       #:recv-status-on-client recv-status))
 
   (define sema (make-semaphore))
-  
+
   (grpc-call-start-batch call ops (malloc-immobile-cell sema))
   (sync sema)
 
@@ -63,11 +63,13 @@
   (case status
     [(0)
      (define recv-message-buffer (ptr-ref recv-message-buffer-ptr _pointer))
+     (grpc-byte-buffer-destroy recv-message-buffer)
+     (grpc-call-destroy call)
      (void)]
     [else
       (printf "Error ~a: ~a~n" status (ptr-ref recv-status_details _string))]))
-  
-  
+
+
 (module+ main
   (define cq (start-completion-queue))
 
