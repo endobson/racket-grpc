1aa2d4f8e145641a75e69a6fe96aca78bfec605f
Eric Dobson <eric.n.dobson@gmail.com>
2017-09-25 21:56:07 -0700

    Add more slice functions.

--- ffi/slice.rkt
+++ ffi/slice.rkt
@@ -15,7 +15,12 @@
     [grpc-slice? (c:-> any/c boolean?)]
     [grpc-slice-from-copied-buffer (c:-> bytes? grpc-slice?)]
     [grpc-slice-unref (c:-> grpc-slice? void?)]
-    [grpc-slice->bytes (c:-> grpc-slice? bytes?)]))
+    [grpc-slice->bytes (c:-> grpc-slice? bytes?)]
+    [grpc-slice->bytes!
+      (c:-> bytes? exact-nonnegative-integer? grpc-slice? exact-nonnegative-integer?
+            exact-nonnegative-integer?)]
+    [grpc-slice-length (c:-> grpc-slice? exact-nonnegative-integer?)]
+    [make-empty-grpc-slice (c:-> grpc-slice?)]))
 
 (define-cstruct _grpc-slice-refcounted
   ([bytes _pointer]
@@ -51,3 +56,33 @@
   (define bytes (make-bytes length))
   (memmove bytes start length)
   bytes)
+
+(define (grpc-slice->bytes! bytes-dest dest-start slice-src slice-start)
+  (define-values (length slice-pointer)
+    (if (grpc-slice-refcount slice-src)
+        (let ([data (union-ref (grpc-slice-data slice-src) 0)])
+          (values
+            (grpc-slice-refcounted-length data)
+            (grpc-slice-refcounted-bytes data)))
+        (let ([data (union-ref (grpc-slice-data slice-src) 1)])
+          (values
+            (grpc-slice-inlined-length data)
+            (array-ptr (grpc-slice-inlined-bytes data))))))
+  (define copy-amount
+    (min
+      (- (bytes-length bytes-dest) dest-start)
+      (- length slice-start)))
+
+  (memmove
+    (ptr-add bytes-dest dest-start)
+    (ptr-add slice-pointer slice-start)
+    copy-amount)
+  copy-amount)
+
+(define (grpc-slice-length slice)
+  (if (grpc-slice-refcount slice)
+      (grpc-slice-refcounted-length (union-ref (grpc-slice-data slice) 0))
+      (grpc-slice-inlined-length (union-ref (grpc-slice-data slice) 1))))
+
+(define (make-empty-grpc-slice)
+  (ptr-ref (malloc _grpc-slice) _grpc-slice))
